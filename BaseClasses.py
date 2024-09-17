94c8464341928eb8f2d8e32d39d24fd022ca0bb2
Reis <80530563+JoaoReis153@users.noreply.github.com>
2024-09-17 09:16:00 +0100

    small changes :)

--- BaseClasses.py
+++ BaseClasses.py
@@ -1,6 +1,5 @@
-import re
-from utils import convert_to_base_unit
-
+import re 
+import math
 
 class Champion:
     def __init__(self, fromString):
@@ -120,16 +119,18 @@ class Answer:
 
 def convert_to_base_unit(input_str, arcList=[]):
     numbers = re.findall(r'\d+', input_str)
-    if not numbers:
-        return None
-
-    content = int("".join(numbers))
-
+    
+        
     if arcList is not None:
         if input_str in arcList:
             return arcList.index(input_str)
-        elif "base64" in input_str:
-            return None
+        #elif "base64" in input_str:
+            #return None
+
+    if not numbers:
+        return None
+        
+    content = int("".join(numbers))
 
     if 'cm' in input_str:
         cm_value = int(re.sub(r'\D', '', input_str))
